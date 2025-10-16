6bc73f35114dbbd2c24e80fe5387376600b0be34
Joe McClure <jdmcclur@us.ibm.com>
2025-10-16 09:05:43 -0500

    Refactor websocket code

--- daytrader-ee7-web/src/main/java/com/ibm/websphere/samples/daytrader/web/websocket/MarketSummaryWebSocket.java
+++ daytrader-ee7-web/src/main/java/com/ibm/websphere/samples/daytrader/web/websocket/MarketSummaryWebSocket.java
@@ -15,12 +15,17 @@
  */
 package com.ibm.websphere.samples.daytrader.web.websocket;
 
-import java.util.Collections;
-import java.util.HashSet;
+import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.enterprise.event.Observes;
 import javax.jms.Message;
 import javax.json.Json;
@@ -48,8 +53,16 @@ import com.ibm.websphere.samples.daytrader.util.WebSocketJMSMessage;
 @ServerEndpoint(value = "/marketsummary",decoders=ActionDecoder.class)
 public class MarketSummaryWebSocket {
 
-	private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+    @Resource
+    private ManagedScheduledExecutorService managedScheduledExecutorService;
+
+	private static final List<Session> SESSIONS = new CopyOnWriteArrayList<>();
+    private static final int SCHEDULER_PERIOD = Integer.parseInt(System.getProperty("dt.ws.period", "2"));
     private final CountDownLatch latch = new CountDownLatch(1);
+
+    private static boolean sendRecentQuotePriceChangeList = false;
+    private static ScheduledFuture<?> scheduler = null;
+
 
     @OnOpen
     public void onOpen(final Session session, EndpointConfig ec) {
@@ -57,7 +70,17 @@ public class MarketSummaryWebSocket {
             Log.trace("MarketSummaryWebSocket:onOpen -- session -->" + session + "<--");
         }
 
-        sessions.add(session);
+        synchronized(SESSIONS) {
+            if (SESSIONS.size() == 0) {
+                 if (Log.doTrace()) {
+                    Log.trace("MarketSummaryWebSocket:onOpen -- start scheduler");
+                 }
+                startScheduler();
+            }
+
+            SESSIONS.add(session);
+        }
+  
         latch.countDown();
     } 
     
@@ -73,32 +96,50 @@ public class MarketSummaryWebSocket {
                 Log.trace("MarketSummaryWebSocket:sendMarketSummary -- received -->null<--");
             }
         }
+
+        // Make sure onopen is finished
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return;
+        }
         
         if (action != null && action.equals("update")) {
             TradeAction tAction = new TradeAction();
-
+                            
+            JsonObject mkSummary = null;
             try {
-                
-                JsonObject mkSummary = tAction.getMarketSummary().toJSON();
-
-                if (Log.doTrace()) {
-                    Log.trace("MarketSummaryWebSocket:sendMarketSummary -- sending -->" + mkSummary + "<--");
-                }
-                
-                // Make sure onopen is finished
-                latch.await();
-                
-                if (RecentStockChangeList.isEmpty()) {
-                    currentSession.getAsyncRemote().sendText(mkSummary.toString());
-                    
-                }
-                else { // Merge Objects 
-                    JsonObject recentChangeList = RecentStockChangeList.stockChangesInJSON();
-                    currentSession.getAsyncRemote().sendText(mergeJsonObjects(mkSummary,recentChangeList).toString());
-                }
-                
+                mkSummary = tAction.getMarketSummary().toJSON();
             } catch (Exception e) {
                 e.printStackTrace();
+                return;
+            }
+
+            if (Log.doTrace()) {
+                Log.trace("MarketSummaryWebSocket:sendMarketSummary -- sending -->" + mkSummary + "<--");
+            }
+                            
+            if (RecentStockChangeList.isEmpty()) {
+                synchronized (currentSession) {
+                    if (currentSession.isOpen()) {
+                        try {
+                            currentSession.getBasicRemote().sendText(mkSummary.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }   
+            }
+            else { // Merge Objects 
+                JsonObject recentChangeList = RecentStockChangeList.stockChangesInJSON();
+                if (currentSession.isOpen()) {
+                    try {
+                        currentSession.getBasicRemote().sendText(mergeJsonObjects(mkSummary,recentChangeList).toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } 
             }
         }
     }
@@ -118,7 +159,15 @@ public class MarketSummaryWebSocket {
             Log.trace("MarketSummaryWebSocket:onClose -- session -->" + session + "<--");
         }
 
-        sessions.remove(session);
+        synchronized(SESSIONS) {
+            SESSIONS.remove(session);
+            if (SESSIONS.size() == 0) {  
+                if (Log.doTrace()) {
+                    Log.trace("MarketSummaryWebSocket:onClose -- cancel scheduler");
+                }
+                scheduler.cancel(false);
+            }
+        }
 
     }
     
@@ -127,19 +176,39 @@ public class MarketSummaryWebSocket {
     	if (Log.doTrace()) {
             Log.trace("MarketSummaryWebSocket:onJMSMessage");
         }
-    	
-    	RecentStockChangeList.addStockChange(message);
-        
+        RecentStockChangeList.addStockChange(message);
+        sendRecentQuotePriceChangeList = true;
+    }    
+
+    private void sendRecentQuotePriceChangeList() {
         JsonObject stockChangeJson = RecentStockChangeList.stockChangesInJSON();
-        
-        synchronized(sessions) {
-            for (Session s : sessions) {
-                if (s.isOpen()) {
-                    s.getAsyncRemote().sendText(stockChangeJson.toString());
+  
+        for (Session session : SESSIONS) {
+            synchronized (session) {
+                if (session.isOpen()) {
+                    try {
+                        session.getBasicRemote().sendText(stockChangeJson.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }      
                 }
             }
         }
-    }    
+    }
+    
+
+    private void startScheduler() {
+		scheduler = managedScheduledExecutorService.scheduleAtFixedRate(() -> {
+        Log.trace("MarketSummaryWebSocket: Executing static scheduled task at: " + System.currentTimeMillis());
+        if (sendRecentQuotePriceChangeList) {
+          Log.trace("MarketSummaryWebSocket: sendList = true");
+          sendRecentQuotePriceChangeList();
+          sendRecentQuotePriceChangeList = false;
+        } else {
+          Log.trace("MarketSummaryWebSocket: sendList = false");
+        }
+      }, 1, SCHEDULER_PERIOD, TimeUnit.SECONDS);
+	}
     
     private JsonObject mergeJsonObjects(JsonObject obj1, JsonObject obj2) {
         
